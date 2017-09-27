ecb733cc1d61fb3f2e5ef55cb4fee1b2f971a897
Kizilov <kizilov.yura@gmail.com>
2017-09-27 17:30:20 +0300

    add user section

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+    /*------------------start login tabs--------------------*/
+
     $('.main__form--content').each(function (i) {
         if(i !=0) {
             $(this).hide(0);
@@ -15,5 +18,56 @@ $(function () {
         $(tabId).fadeIn();
     });
 
+    /*------------------end login tabs--------------------*/
+
+
+
+
+
+
+    /*------------------start header tabs--------------------*/
+
+    $('.adm-content__section').each(function (i) {
+        if(i !=0) {
+            $(this).hide(0);
+        }
+    });
+
+    $(document).on('click', '.adm-header__menu a', function (e) {
+        e.preventDefault();
+        var tabId = $(this).attr('href');
+        $('.adm-header__menu a').removeClass('adm-header__menu--active');
+        $(this).addClass('adm-header__menu--active');
+        $('.adm-content__section').hide(0);
+        $(tabId).fadeIn();
+    });
+
+    /*--------------------end header tabs--------------------*/
+
+
+
+
+
+    /*-------------------start action-btn---------------------*/
+
+    $('.action-btn--text').text($('.action-btn ul li:first-of-type a').text());
+
+    $(document).on('click', '.action-btn--drop', function () {
+        $(this).toggleClass('action-btn--active');
+        $(this).parent().find('ul').fadeToggle('fast');
+    });
+
+    $(document).on('click', '.action-btn ul li a', function () {
+        $('.action-btn--drop').removeClass('action-btn--active');
+        $(this).parent().parent().parent().find($('.action-btn--text').text($(this).text()));
+        $('.action-btn ul').fadeToggle('fast')
+    });
+
+    $(document).click(function(event) {
+        if ($(event.target).closest(".action-btn").length) return;
+        $(".action-btn ul").fadeOut("fast");
+    });
+
+    /*---------------------end action-btn---------------------*/
 
 });
