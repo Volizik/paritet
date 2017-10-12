c2151816a39a245cd31cf875a2043a8501029e60
Kizilov <kizilov.yura@gmail.com>
2017-10-12 18:00:56 +0300

    add groups-cabinet page

--- app/js/modal.js
+++ app/js/modal.js
@@ -2,16 +2,19 @@ var modal = document.querySelector('.modal-delete');
 var btn = document.querySelector('.delete');
 var span = document.querySelector('.modal__header>.t-delete');
 var cancel = document.querySelector('.modal__footer>.cancel');
-btn.onclick = function(e) {
+$(document).on('click', btn, function (e) {
     e.preventDefault();
     modal.style.display = "block";
-}
-span.onclick = function() {
+})
+$(document).on('click', span, function () {
     modal.style.display = "none";
-}
-cancel.onclick = function() {
+})
+$(document).on('click', cancel, function () {
     modal.style.display = "none";
-}
+})
+$(document).on('click', cancel, function () {
+    modal.style.display = "none";
+})
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
